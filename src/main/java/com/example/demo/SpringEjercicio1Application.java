8fe7d9b3bc1c9e6a5280f64fe320cc9e8ebe2f1e
NicoHndzHc <nico.hndz.s@gmail.com>
2023-07-21 21:28:31 -0400

    Estructura base para proyecto Spring Java

---
+++ src/main/java/com/example/demo/SpringEjercicio1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEjercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEjercicio1Application.class, args);
+	}
+
+}
